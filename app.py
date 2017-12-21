6acbb081f6d94af110ee14f55c6815da99921322
trety <tretyak.olesya.lviv@gmail.com>
2017-12-21 17:39:30 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+from data import Categories, Options
+
+app = Flask(__name__)
+
+Categories = Categories()
+Options = Options()
+
+@app.route('/')
+def index():
+    return render_template('home.html', options=Options)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/categories')
+def categories():
+    return render_template('categories.html', categories = Categories)
+
+@app.route('/category/<string:id>/')
+def category(id):
+    return render_template('category.html', id=id)
+
+@app.route('/input', methods = ["POST", "GET"])
+def input():
+    if request.method == "POST":
+        dictionary = {'їжа':0}
+        money = request.form['money']
+        value = request.form['User_choice[]']
+        return render_template('true.html', value=value)
+    else:
+        return render_template('input.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html', id=id)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,106 @@
+def Categories():
+    categories = [
+        {    'id': 1,
+            'title': 'Їжа',
+            'body': 'ug',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 2,
+            'title': 'Одяг',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 3,
+            'title': 'Транспорт',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 4,
+            'title': "Мобільний зв'язок",
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 5,
+            'title': 'Розваги',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 6,
+            'title': 'Спорт',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 7,
+            'title': 'Подарунки',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 8,
+            'title': 'Оплата житла',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 9,
+            'title': 'Комунальні послуги',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 10,
+            'title': 'Подорожі',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 11,
+            'title': 'Догляд за собою',
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+        {    'id': 12,
+            'title': "Здоров'я",
+            'body': 'Lorem nwivnpweonvpwovmw',
+            'author': 'Logan Paul',
+            'create_date': '25-04-2017'
+            },
+    {    'id': 13,
+        'title': 'Товари для дому',
+        'body': 'Lorem nwivnpweonvpwovmw',
+        'author': 'Logan Paul',
+        'create_date': '25-04-2017'
+        },
+    {    'id': 14,
+        'title': 'Канцелярія',
+        'body': 'Lorem nwivnpweonvpwovmw',
+        'author': 'Logan Paul',
+        'create_date': '25-04-2017'
+        },
+    {    'id': 15,
+        'title': 'Освіта(курси і тд)',
+        'body': 'Lorem nwivnpweonvpwovmw',
+        'author': 'Logan Paul',
+        'create_date': '25-04-2017'
+        },
+    {    'id': 16,
+        'title': 'Інше',
+        'body': 'Lorem nwivnpweonvpwovmw',
+        'author': 'Logan Paul',
+        'create_date': '25-04-2017'
+        }
+    ]
+    return categories
+def Options():
+    options = [
+    {'id':"input", 'title':"Ввід" },
+    {'id':"view", 'title':"Переглянути"}
+    ]
+    return options
