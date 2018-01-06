619681ce8671fb0a6583fd8b50eff64e3fbb254f
dariya24 <darynareshetukha@gmail.com>
2018-01-06 16:51:12 +0200

    Google Authentification is added

--- app.py
+++ app.py
@@ -1,16 +1,85 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_oauth import OAuth
+import json
 from data import Categories, Options
 import csv
 import datetime
 
+USERNAME = None;
+FILENAME = None;
 app = Flask(__name__)
 
 Categories = Categories()
 Options = Options()
 
-@app.route('/')
+GOOGLE_CLIENT_ID = '359520140035-0nlqnjo2g2daujj0algii7m5fff21hif.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'p9MkeBPqw0lob_Tke1BZsX8N'
+REDIRECT_URI = '/authorized'  # one of the Redirect URIs from Google APIs console
+SECRET_KEY = 'development key'
+app.secret_key = SECRET_KEY
+oauth = OAuth()
+google = oauth.remote_app('google',
+                          base_url='https://www.google.com/accounts/',
+                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          request_token_url=None,
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                                                'response_type': 'code'},
+                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_method='POST',
+                          access_token_params={'grant_type': 'authorization_code'},
+                          consumer_key=GOOGLE_CLIENT_ID,
+                          consumer_secret=GOOGLE_CLIENT_SECRET)
+
 def index():
-    return render_template('home.html', options=Options)
+    global USERNAME, FILENAME
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+    access_token = access_token[0]
+    from urllib.request import Request, urlopen
+    from urllib.error import URLError
+    headers = {'Authorization': 'OAuth '+access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        res = urlopen(req)
+    except URLError as e:
+        if e.code == 401:
+            # Unauthorized - bad token
+            session.pop('access_token', None)
+            return redirect(url_for('login'))
+        return res.read()
+    json_string = res.read().decode('utf-8')
+    json_obj = json.loads(json_string)
+    USERNAME = json_obj['name']
+    FILENAME = json_obj['email'][:json_obj['email'].index('@')]
+    FILENAME = str(FILENAME)+'.csv'
+    return redirect(url_for('home'))
+
+@app.route('/login')
+def login():
+    callback=url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return index()
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
+
+@app.route('/')
+def start():
+    return redirect(url_for('login'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html', options=Options, name=USERNAME)
 
 @app.route('/about')
 def about():
@@ -28,7 +97,7 @@ def category(id):
 def input():
     if request.method == "POST":
         lst_cat = ['food', 'clothes', 'transportation', 'phone', 'fun', 'sport', 'gifts', 'rent', 'utilities', 'travel', 'personalcare', 'health', 'housing', 'supplies', 'education', 'other']
-        with open('text.csv', 'a', newline='') as csvfile:
+        with open(FILENAME, 'a', newline='') as csvfile:
             swriter = csv.writer(csvfile, delimiter=',')
             for i in lst_cat:
                 x = [int(e) for e in request.form[i].split()]
@@ -42,4 +111,4 @@ def view():
     return render_template('view.html', id=id)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run()
